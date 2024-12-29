176e17aab88fd3b93fbdef1af76a274f3be5bddc
AnuragWakchaure <151008646+AnuragWakchaure@users.noreply.github.com>
2024-12-29 20:51:11 +0530

    Add files via upload

---
+++ IP Calculator/rangecal/iprange.py
@@ -0,0 +1,6 @@
+from brcal import broad as a
+from netcal import nwadd as n
+
+def iprange(uservar,uservar1,uservar2,uservar3):
+    print(f"usable range  is {uservar}.{uservar1}.{uservar2}.1 to {uservar}.{uservar1}.{uservar2}.{uservar3}")
+     
