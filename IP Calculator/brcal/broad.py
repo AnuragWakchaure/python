deecff10d1f52b578d31a422e7bc843b54c338d6
AnuragWakchaure <151008646+AnuragWakchaure@users.noreply.github.com>
2024-12-29 20:49:25 +0530

    Add files via upload

---
+++ IP Calculator/brcal/broad.py
@@ -0,0 +1,6 @@
+def cal_bro_add(uservar,uservar1,uservar2,uservar3):
+    bro_use=uservar|0
+    bro_use1=uservar1|0
+    bro_use2=uservar2|0
+    bro_use3=uservar3|255
+    print(f"broadcast address is {bro_use}.{bro_use1}.{bro_use2}.{bro_use3}")
